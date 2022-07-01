a5ad9bc493a32b62e494025daa9ac0c11f470105
VadimKiselev <mr.vkiselev@gmail.com>
2022-07-01 16:58:02 +0300

    bonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int balans = 250; // баланс
+        int balansAdd = 1250; // пополнение
+        int balansOver; // итоговый баланс
+        int bonus = 30; //бонусный баланс
+        int bonusAdd; //начислено бонусов
+        int bonusOver; //итоговое количество бонусов
+
+        if (balansAdd >= 1000) {
+            bonusAdd = balansAdd / 100;
+        } else {
+            bonusAdd = 0;
+        }
+        balansOver = balans + balansAdd;
+        bonusOver = bonus + bonusAdd;
+
+        System.out.println("Ваш баланс: " + balansOver);
+        System.out.println("Количество бонусов " + bonusOver);
     }
+
 }
